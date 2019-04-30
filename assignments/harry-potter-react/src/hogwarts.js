a71359b25953522b53e58e989f81a685fd058ce6
Jackie Casper <casper.jaclyn@gmail.com>
2019-04-30 13:11:12 +0300

    add lesson

---
+++ assignments/harry-potter-react/src/hogwarts.js
@@ -0,0 +1,89 @@
+export default {
+  location: 'Scotland',
+  houses: [
+    {
+      name: 'Griffindor',
+      img: 'http://images1.wikia.nocookie.net/__cb20120922143611/harrypotter/images/7/70/G_final.jpg',
+      values: [
+        'courage',
+        'bravery',
+        'nerve',
+        'chivalry'
+      ],
+      mascot: 'lion',
+      colors: [
+        'scarlet',
+        'gold'
+      ],
+      founder: 'Godric Gryffindor',
+      students: [
+        'Harry Potter',
+        'Hermione Granger',
+        'Ron Weasley'
+      ]
+    },
+    {
+      name: 'Hufflepuff',
+      img: 'http://images3.wikia.nocookie.net/__cb20111027164827/harrypotter/images/3/3f/H_final.jpg',
+      values: [
+        'hardwork',
+        'patience',
+        'justice',
+        'loyalty'
+      ],
+      mascot: 'badger',
+      colors: [
+        'canary yellow',
+        'black'
+      ],
+      founder: 'Helga Hufflepuff',
+      students: [
+        'Cedric Diggory',
+        'Susan Bones',
+        'Nymphadora Tonks'
+      ]
+    },
+    {
+      name: 'Ravenclaw',
+      img: 'http://images2.wikia.nocookie.net/__cb20111021043857/harrypotter/images/d/da/R_final.jpg',
+      values: [
+        'intelligence',
+        'creativity',
+        'learning',
+        'wit'
+      ],
+      mascot: 'eagle',
+      colors: [
+        'blue',
+        'bronze'
+      ],
+      founder: 'Rowena Ravenclaw',
+      students: [
+        'Luna Lovegood',
+        'Cho Chang',
+        'Padma Patil'
+      ]
+    },
+    {
+      name: 'Slytherin',
+      img: 'http://images3.wikia.nocookie.net/__cb20111027165214/harrypotter/images/d/da/S_final.jpg',
+      values: [
+        'ambition',
+        'cunning',
+        'leadership',
+        'resourcefulness'
+      ],
+      mascot: 'serpent',
+      colors: [
+        'green',
+        'silver'
+      ],
+      founder: 'Salazar Slytherin',
+      students: [
+        'Draco Malfoy',
+        'Pansy Parkinson',
+        'Tom Riddle'
+      ]
+    }
+  ]
+}
